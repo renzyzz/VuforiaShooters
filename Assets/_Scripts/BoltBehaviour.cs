b47d0f300d8d39084b361dbff507f94054a63990
Unknown <repierrard@gmail.com>
2017-12-18 12:43:57 +0100

    Harsh but playable

---
+++ Assets/_Scripts/BoltBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Asteroid")
+        {
+            Destroy(other.gameObject);
+            Scoring.point += 5;
+            transform.localScale -= new Vector3(0.1F, 0.1f, 0.1f);
+        }
+        else if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Scoring.point += 10;
+            transform.localScale -= new Vector3(0.1F, 0.1f, 0.1f);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Enemy;
+
+    [SerializeField]
+    GameObject Astero;
+
+    [SerializeField]
+    GameObject Spawncube;
+
+    float Countdown;
+    float LifeTime = 2f;
+    bool Isdead = false;
+    float danger = 0;
+
+    private void Awake()
+    {
+        Countdown = LifeTime;
+        EnemyBehaviour.isdead = false;
+        Scoring.point = 0;
+    }
+	
+	void Update ()
+    {
+        if (Countdown <= 0)
+
+        {
+            Vector3 sposition = new Vector3(Random.Range(-2000.0f, 2000.0f), 204f, Random.Range(-2000.0f, 2000.0f));
+            Instantiate(Astero, sposition, Spawncube.transform.rotation);
+            Countdown = LifeTime;
+            if (danger % 3 == 0)
+            {
+                Vector3 sposition2 = new Vector3(Random.Range(-2000.0f, 2000.0f), 204f, Random.Range(-2000.0f, 2000.0f));
+
+                Instantiate(Enemy, sposition2, Spawncube.transform.rotation);
+
+
+            }
+            danger++;
+        }
+        else
+        {
+            Countdown -= Time.deltaTime;
+        }
+
+
+
+
+    }
+}
